ea1a8537f1742dc0c30d1a0c5ab6f048556315fd
shirleyR <shuilinren@foxmail.com>
2018-01-07 23:19:13 +0800

    add babelrc for support react and es2015, change demo

---
+++ app/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../src';
+import { render } from 'react-dom';
+render( <Layout container justify="center" alignItems="flex-end">
+    <Layout item style={{height:"100px"}} >
+        <div>{"Option3"}</div>
+    </Layout>
+    <Layout item style={{width:"30px"}}>
+         <div>{"Option4"}</div>
+    </Layout>
+    <Layout item style={{height:"40px"}}>
+        <div>{"Option7"}</div>
+    </Layout>
+</Layout>, document.getElementById("root"))
